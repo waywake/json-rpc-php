710313a1b3124d47c75e7ca48fa73c464cdf9ced
dongwei <838456736@qq.com>
2019-02-12 15:53:33 +0800

    生产环境不产生

--- src/Middleware/TunnelMiddleware.php
+++ src/Middleware/TunnelMiddleware.php
@@ -39,7 +39,7 @@ class TunnelMiddleware
 	{
 		//过滤tool返回结果
 		if ($response instanceof JsonResponse) {
-			if (app()->environment('dev', 'production')) {
+			if (app()->environment('dev')) {
 				$content = $response->getOriginalContent();
 				$status = isset($content['error']) ? $content['error']['code'] : 200;
 				$client = new \InfluxDB\Client("10.0.1.67");
